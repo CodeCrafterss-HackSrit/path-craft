58798bd53440eefa1640f634e70092d44c55f25e
dvnshi <devanshijain460@gmail.com>
2025-05-11 06:57:43 +0530

    updates

--- app/_custom/Footer.jsx
+++ app/_custom/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="flex flex-col md:flex-row justify-between items-center gap-2 w-full p-24 h-auto text-lg bg-gray-100">
-      <div className="text-center">Crafted by Abhishek Allaboina</div>
+      {/* <div className="text-center">Crafted by Abhishek Allaboina</div>
 
       <div className="flex items-center gap-2">
         <a
@@ -25,7 +25,7 @@ function Footer() {
         >
           LinkedIn
         </a>
-      </div>
+      </div> */}
     </footer>
   );
 }
